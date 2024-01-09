56f1132e22205286ad128d083aaf843028fdaf36
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-01-09 19:10:21 +0100

    traccia 1

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+//traccia 1
+
+$intero = 11;
+$float = 1.5;
+$stringa = "Primo esercizio php!";
+$booleano = true;
+
+var_dump($intero);
+var_dump($float);
+var_dump($stringa);
+var_dump($booleano);
