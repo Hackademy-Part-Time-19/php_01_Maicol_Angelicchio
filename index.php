49f7a4a5af050fa52dea9be6d258c62b1bbd3413
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-01-09 19:17:46 +0100

    traccia 2

--- index.php
+++ index.php
@@ -11,3 +11,18 @@ var_dump($intero);
 var_dump($float);
 var_dump($stringa);
 var_dump($booleano);
+
+
+
+//traccia 2
+
+$text1="Marco";
+$text2="hai";
+$text3="sete";
+$text4="?";
+$text5="Perchè";
+$text6='$text2';
+$text7='bevuto';
+$text8="tutto";
+
+echo $text1 . ' ' . $text2 . ' ' . $text3 . $text4 . ' ' . $text5 . ' ' . $text2 . ' ' . $text7 . ' ' . $text8;
