395a6f5c15324da90f17452d008fc90068b0f970
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-01-09 19:47:25 +0100

    esercizio 3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 //traccia 1
-
+/*
 $intero = 11;
 $float = 1.5;
 $stringa = "Primo esercizio php!";
@@ -26,3 +26,42 @@ $text7='bevuto';
 $text8="tutto";
 
 echo $text1 . ' ' . $text2 . ' ' . $text3 . $text4 . ' ' . $text5 . ' ' . $text2 . ' ' . $text7 . ' ' . $text8;
+
+*/
+//Traccia 3
+
+$words1=[
+    'una',
+    67,
+    'vita',
+    'colle',
+    'mi',
+    'rosso',
+        ['oscura',
+        'era',
+        89,
+            ['mezzo',
+                ['cammin',
+                'Nel',
+                    ['selva',
+                    'la',
+                        ['via',
+                        'una',
+                        true,
+                        ]
+                    ],
+                ]
+            ],
+            'ritrovai','per'
+        ],'diritta'
+    ];
+
+$words2=['elemento1'=>25.89,'elemento2'=>'nostra','elemento3'=>['Virgilio','smarrita','ché']];
+
+$words3=[' ','di',',','.'];
+
+$results = $words1[6][3][1][1].$words3[0].$words1[6][3][0].$words3[0].$words3[1].$words3[0].$words1[6][3][1][0].$words3[0].$words3[1].$words3[0]
+.$words2['elemento2'].$words3[0].$words1[2].$words3[0].$words1[4].$words3[0].$words1[6][4].$words3[0].$words1[6][5].$words3[0].$words1[0]
+.$words3[0].$words1[6][3][1][2][0].$words3[0].$words1[6][0].$words3[2].$words3[0].$words2['elemento3'][2].$words3[0].$words1[6][3][1][2][1]
+.$words3[0].$words1[7].$words3[0].$words1[6][3][1][2][2][0].$words3[0].$words1[6][1].$words3[0].$words2['elemento3'][1].$words3[3];
+echo $results;
